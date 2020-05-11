6d46502123dbeb6b0a7f3924c2b26b1e72942161
Daniel Tognon <xone.tognon@outlook.com>
2020-05-11 14:49:28 -0300

    Creating components

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomForm from '../CustomForm/CustomForm.js';
+import './App.css';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <CustomForm/>
+        );
+    }
+}
---
+++ src/components/CustomForm/CustomForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './CustomForm.css';
+
+export default class CustomForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log('A name was submitted: ' + this.state.value);
+    }
+
+    render() {
+        return (
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+                <label> Name: </label>
+                <input type="text" value={this.state.value} onChange={(e) => this.handleChange(e)}/>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
+}
